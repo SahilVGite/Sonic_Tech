0f123cce580c0cb24d7207511051cf7b4b581895
SahilGite4fox <sahilgite.4fox@gmail.com>
2025-04-24 13:26:29 +0530

    .

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
 
+  
+  // Sticky Header
+  $(window).on("scroll", function () {
+    $("header").toggleClass("stickyHead", $(this).scrollTop() > 0);
+  });
+
   // Hamburger Menu
   $(".meanuSecBtn").on("click", function () {
     $(".meanuSecBtnNav").toggleClass("active");
@@ -33,139 +39,5 @@ $(document).ready(function () {
   $("img, iframe").attr("loading", "lazy");
 
 
-  // Slick Sliders
-  $(".investingNewsSlider").slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    infinite: true,
-    autoplay: true,
-    dots: true,
-    arrows: true,
-    autoplaySpeed: 4000,
-    responsive: [
-      {
-        breakpoint: 1120,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        }
-      },
-      {
-        breakpoint: 800,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        }
-      }
-    ]
-  });
-
-  $(".webStoriesSlider").slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    infinite: true,
-    autoplay: true,
-    dots: true,
-    arrows: true,
-    autoplaySpeed: 4000,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        }
-      },
-      {
-        breakpoint: 800,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        }
-      }
-    ]
-  });
-
-  $(".hmBannerTop1Slider").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    infinite: true,
-    autoplay: true,
-    dots: true,
-    arrows: false,
-    autoplaySpeed: 4000
-  });
-
-  $(".trTopTags > ul").slick({
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    infinite: true,
-    autoplay: true,
-    dots: false,
-    arrows: true,
-    autoplaySpeed: 4000,
-    responsive: [
-      {
-        breakpoint: 1300,
-        settings: {
-          slidesToShow: 5,
-          slidesToScroll: 1,
-        }
-      },
-      {
-        breakpoint: 1000,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 1,
-        }
-      },
-      {
-        breakpoint: 800,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        }
-      }
-    ]
-  });
-
-
-  // Video PopUp
-  $('.popup-youtube').magnificPopup({
-    disableOn: 700,
-    type: 'iframe',
-    mainClass: 'mfp-fade',
-    removalDelay: 160,
-    preloader: false,
-
-    fixedContentPos: false
-  });
-
-
-  // Sticky Header
-  $(window).on("scroll", function () {
-    $("header").toggleClass("stickyHead", $(this).scrollTop() > 50);
-  });
 
 });
